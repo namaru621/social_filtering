7eeed24bb3275d8f7cbbcfefaa8c92584010b120
Yusuke Kanamaru <Kanamaru@dhcp17.cl.ics.tut.ac.jp>
2018-03-29 14:41:50 +0900

    add new file

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,9 +3,16 @@ class UsersController < ApplicationController
   end
 
   def show
-    @user = Hash.new
-    @user[:name] = 'namarrote'
-    @user[:location] = 'Aichi'
-    @user[:about] = 'I like ...'
+    if params[:username] == 'namaru621'
+      @user = Hash.new
+      @user[:name] = 'namarrote'
+      @user[:location] = 'Aichi'
+      @user[:about] = 'I like ...'
+    elsif params[:username] == 'namaru'
+      @user = Hash.new
+      @user[:name] = 'namaru dayo ~'
+      @user[:location] = 'Aichi'
+      @user[:about] = 'I like ...'
+    end
   end
 end
