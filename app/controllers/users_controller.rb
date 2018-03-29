4a0427a807bf287038b4f123d7dec02c501bd2b5
Yusuke Kanamaru <namaru621@yahoo.co.jp>
2018-03-29 10:53:23 +0900

    add new file

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,5 +3,9 @@ class UsersController < ApplicationController
   end
 
   def show
+    @user = Hash.new
+    @user[:name] = 'namarrote'
+    @user[:location] = 'Aichi'
+    @user[:about] = 'I like ...'
   end
 end
